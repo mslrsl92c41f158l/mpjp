168f76e8727528f201f18af8479a48ada0030782
R M <rossella.musolino@gmail.com>
2019-12-16 09:05:15 +0100

    lunedì 16

--- src/test/java/s054/SimpleTest.java
+++ src/test/java/s054/SimpleTest.java
@@ -6,13 +6,22 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import org.junit.jupiter.api.Test;
 
 class SimpleTest {
-    @Test
-    public void negatePositive() {
-        Simple simple = new Simple();
-        int value = 42;
+	private Simple simple;
 
-        int result = simple.negate(value);
+//	@BeforeEach
+//	public void init() {
+//	simple = new Simple();
+//	}
 
-        assertThat(result, equalTo(-42));
-    }
+	@Test
+	public void negatePositive() {
+		Simple simple = new Simple();
+		int value = 42;
+
+		int result = simple.negate(value);
+
+		assertThat(result, equalTo(-42));
+
+	}
+
 }
